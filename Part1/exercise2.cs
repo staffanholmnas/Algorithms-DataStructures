33df8efc3555557d6bf3597c686b507dcfef9e3b
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-08 12:05:42 +0300

    Exercise 2 Done

--- Part1/exercise2.cs
+++ Part1/exercise2.cs
@@ -26,3 +26,13 @@ namespace Part1
         }
     }
 }
+/*
+
+I want to go through string a with string b, starting at the first character and then the next character the second time, 
+then the third character and so on... I want to stop when the last character of b reaches the last character of a. 
+The code above does this by putting each character from string a to an array, then building new words that have
+the same length as b, and that are put in a new array. Each new word moves up the string by 1 character. These new words
+can then be compared with string b, if they match, 1 is added to sum. Not the most elegant solution, but it works.
+Tested with both large and small strings for both a and b.
+
+*/
