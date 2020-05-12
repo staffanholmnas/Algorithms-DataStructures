59c4e57d89ea215354737a5d8011c9db2e8ff323
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-12 14:07:14 +0300

    Part 1 Done

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -7,10 +7,15 @@ namespace Part1
         static void Main(string[] args)
         {
             LuckyNumbers luck = new LuckyNumbers();
-            Console.WriteLine(luck.Calculate(100, 10000));
-           // Console.WriteLine(luck.Calculate(1, 10)); // 2
-           // Console.WriteLine(luck.Calculate(123, 321)); // 0
-           // Console.WriteLine(luck.Calculate(1, 1000000)); // 126
+            Console.WriteLine(luck.Calculate(1, 10)); // 2
+            Console.WriteLine(luck.Calculate(123, 321)); // 0
+            Console.WriteLine(luck.Calculate(1, 1000000)); // 126
+
+            Console.WriteLine(luck.Calculate(73350, 33373100)); // 222
+            Console.WriteLine(luck.Calculate(36000, 733777)); // 64
+            Console.WriteLine(luck.Calculate(3500, 700000)); // 76
+            Console.WriteLine(luck.Calculate(75, 37737377)); // 357
+            Console.WriteLine(luck.Calculate(0, 300000000)); // 510
         }
     }
 }
--- Part1/exercise4.cs
+++ Part1/exercise4.cs
@@ -5,54 +5,141 @@ namespace Part1
     {
         public int Calculate(int a, int b)
         {
-            Console.WriteLine("---------------");
-            int sum = 0;
-            int sum2 = 0;
+            DateTime start = DateTime.Now;
+
+            Console.WriteLine("------------------------------");
+
+            int sumA = 0;
+            int sumB = 0;
             int total = 0;
 
-            for (int i = 1; i < a; i *= 10)
+            if (a <= 7)
             {
-                sum =  2 + sum * 2; 
-                Console.WriteLine (sum);
-            }    
-            Console.WriteLine("---------------");
-            for (int i = 1; i < b; i *= 10)
+                sumA = CheckInterval(0, a);
+            }
+            else
             {
-                sum2 =  2 + sum2 * 2; 
-                Console.WriteLine (sum2);
-            }    
+                sumA = GetAlgo(a);
+            }
+            if (b <= 7)
+            {
+                sumB = CheckInterval(0, b);
+            }
+            else
+            {
+                sumB = GetAlgo(b);
+            }
 
-            total = sum2 - sum;
-            Console.WriteLine("---------------");
+            sumB = GetNarrowInterval(b, sumB);
+
+            sumA = GetNarrowInterval(a, sumA);
+
+            total = sumB - sumA;
+
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Time elapsed: " + (end.Ticks - start.Ticks) / 10000.0 + " milliseconds");
+
             return total;
+
+        }
+        public static int GetAlgo(int a)
+        {
+            // Used to quikly calculate lucky numbers. For example: numbers outside of 33...33 and 77...77,
+            // or numbers between 37...7 and 73...3.
+
+            int sum = 0;
+            for (int i = 10; i <= a; i *= 10)
+            {
+                sum = 2 + sum * 2;
+            }
+            return sum;
+        }
+        public static int CheckInterval(int a, int b)
+        {
+            // Used for iterating through numbers in small intervals e.g. 7773, 7777.
+            // Not used for larger series, because it is VERY slow.
+
+            int sum = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (!i.ToString().Contains(1.ToString()) && !i.ToString().Contains(2.ToString()) &&
+                !i.ToString().Contains(4.ToString()) && !i.ToString().Contains(5.ToString())
+                && !i.ToString().Contains(6.ToString()) && !i.ToString().Contains(8.ToString())
+                && !i.ToString().Contains(9.ToString()) && !i.ToString().Contains(0.ToString()))
+                {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+        public static int GetBaseNumber(int a)
+        {
+            int sum = 1;
+            for (int i = 10; i <= a; i *= 10)
+            {
+                sum *= 10;
+            }
+            return sum;
+        }
+
+        public static int GetNarrowInterval(int c, int sumC)
+        {
+            // Creates narrow intervals so that the algorithm above can be used for most numbers.
             
+            int number = GetBaseNumber(c);
+
+            double number1 = 0;
+            double number2 = 0;
+            int number3 = 0;
+
+            number1 = number * ((double)10 / 3);
+            number1 = Convert.ToInt32(number1); // Creates lower bound, e.g. 33333
+            number2 = number * ((double)70 / 9);
+            number2 = Convert.ToInt32(number2); // Creates upper bound, e.g. 77777
+            number2--;
+
+            if (c < number1)
+            {
+                sumC = GetAlgo(number);
+            }
+
+            if (c > number2)
+            {
+                sumC = GetAlgo(number * 10);
+            }
+
+            if (c >= number1 && c <= number2)
+            {
+
+                for (int i = 1; i < number; i *= 10)
+                {
+                    number3 = Convert.ToInt32(number1);
+
+                    number1 = number1 + (number / i) * 4; // Creates next upper bound, e.g. 37777, and so on...
+
+                    if (c >= number1 && c <= number2)
+                    {
+                        sumC += ((GetAlgo(number * 10)) - (GetAlgo(number))) / 2;
+                        sumC += CheckInterval(Convert.ToInt32(number1), c);
+                    }
+
+                    number2 = number2 - (number / i) * 4; // Creates next lower bound, e.g. 73333, and so on...
+
+                    if (c >= number3 && c <= number2)
+                    {
+                        sumC += CheckInterval(number3, c);
+                    }
+
+                    if (i == 1) // If number is between e.g. 3777 and 7333, caluculate using only algorithms.
+                    {
+                        if (c > number2 && c < number1) 
+                        {
+                            sumC += ((GetAlgo(number * 10)) - (GetAlgo(number))) / 2; 
+                        }
+                    }
+                }
+            }
+            return sumC;
         }
     }
 }
-
-
-/*  if (b >= 10)
-            {
-                sum += 2;
-            }
-            if (b >= 100)
-            {
-                sum += 4;
-            }
-            if (b >= 1000)
-            {
-                sum += 8;
-            }
-            if (b >= 10000)
-            {
-                sum += 16;
-            }  */
-
-          /*  int index = 1;
-            while (index <= b)
-            {
-                sum2 *= 2; 
-                index *= 10;
-            }*/
-            
-//sum2 = 2 + sum2 * 2; 
