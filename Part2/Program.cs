e4f8740d8ffbf1ddfbd12be9ddab6c508eac581e
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-14 19:22:33 +0300

    Exercise 5

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -6,10 +6,10 @@ namespace Part2
     {
         static void Main(string[] args)
         {
-            Revolutions r = new Revolutions();
-            Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
-            Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-            Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
+            Split s = new Split();
+            Console.WriteLine(s.Calculate(new int[] { 1, 2, -1, 4, 0 })); // 1
+            Console.WriteLine(s.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 0
+            Console.WriteLine(s.Calculate(new int[] { 0, 0, 0, 0, 0 })); // 4
         }
     }
 }
---
+++ Part2/exercise5.cs
@@ -0,0 +1,13 @@
+namespace Part2
+{
+    public class Split
+    {
+        public int Calculate(int[] t)
+        {
+            int sum = 0;
+
+
+            return sum;
+        }   
+    }
+}
