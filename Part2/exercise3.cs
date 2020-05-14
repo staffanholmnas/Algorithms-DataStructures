dc8d5e454fb9666fab98d7dedda28e37d734bc32
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-14 22:40:06 +0300

    Exercise 3 Done

--- Part2/exercise3.cs
+++ Part2/exercise3.cs
@@ -8,13 +8,16 @@ namespace Part2
 
             for (int i = 1; i <= t.Length - 1; i++)
             {
+                // If a number is the same as the one before it...
                 if (t[i] == t[i - 1])
                 {
+                    // ...add the adjacent numbers together. Add 1 to sum.
                     if (i < t.Length - 1)
                     {
                         t[i] = t[i - 1] + t[i + 1];
                         sum++;
                     }
+                    // Unless it's the last integer, then add only the previous one. Add 1 to sum.
                     else
                     {
                         t[i] = t[i - 1] + t[i];
@@ -22,6 +25,7 @@ namespace Part2
                     }
                 }
             }
+
             return sum;
         }
     }
