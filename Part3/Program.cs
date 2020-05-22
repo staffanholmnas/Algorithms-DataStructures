3375819c2339ec81d1301052a94022f73cb0931f
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-22 12:26:41 +0300

    Exercise 4 Done

--- Part3/Program.cs
+++ Part3/Program.cs
@@ -6,23 +6,12 @@ namespace Part3
     {
         static void Main(string[] args)
         {
-            /*
-            Sorting s = new Sorting();
-            int[] sortMe = Randomizer(4);
-            int[] sortMeLarge = Randomizer(1000000);
-            int[] example = new int[] {5,1,2,9};
-            s.QuickSort(example);
-          //  s.MergeSort(example);
-          //  s.QuickSort(sortMeLarge);
-          //  s.MergeSort(sortMeLarge);*/
             BinarySearch b = new BinarySearch();
-            int[] example = new int[] { 4, 1, 8, 5 };
             Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5 }, 2)); // false
             Console.WriteLine(b.Find(new int[] { 0, 0 }, 0)); // true
             Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }, 2)); // true
             Console.WriteLine(b.Find(new int[] { 0 }, 0)); // true
-            Console.WriteLine(b.Find(Randomizer(100000), 3)); // ?
-
+            Console.WriteLine(b.Find(Randomizer(100000), 3)); // Most likely true
         }
         public static int[] Randomizer(int n)
         {
--- Part3/exercise4.cs
+++ Part3/exercise4.cs
@@ -7,18 +7,27 @@ namespace Part3
         public bool Find(int[] t, int x)
         {
             Array.Sort(t);
-            int k = (t.Length / 2);
-
-            while (true)
+        
+            int a = 0;
+            int b = t.Length - 1;
+            
+            while (a <= b)
             {
+                int k = ((a + b) / 2);
                 if (t[k] == x)
                 {
-                    return true;
+                   return true;
                 }
-                
+                if (t[k] < x)
+                {
+                    a = k + 1;
+                }
+                if (t[k] > x)
+                {
+                    b = k - 1;
+                }
             }
-
-            
+            return false;
         }
     }
 }
