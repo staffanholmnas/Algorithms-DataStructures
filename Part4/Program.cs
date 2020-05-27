3027eea6088bcb7de9f8eb6b3baf431553deb927
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-27 16:36:45 +0300

    create Part 4

---
+++ Part4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Part4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList myLinks = new LinkedList();
+            myLinks.AddLast(1);
+            myLinks.AddFirst(2);
+            myLinks.AddLast(3);
+            Console.WriteLine(myLinks); // for example: 2, 1, 3
+            myLinks.RemoveFirst();
+           // Console.WriteLine(myLinks); // for example: 1, 3
+            //Console.WriteLine(myLinks.GetNode(0)); // 1
+            //Console.WriteLine(myLinks.GetNode(1)); // 3
+        }
+    }
+}
---
+++ Part4/exercise1/LinkedList.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace Part4
+{
+    
+    public class LinkedList
+    {
+        public Node node;
+
+        public LinkedList()
+        {
+            this.node = new Node(0,null,null);
+        }
+
+        public void AddFirst(int n)
+        {
+           this.node.value = n;
+           
+        }
+        public void AddLast(int n)
+        {
+
+        }
+        public void RemoveFirst()
+        {
+
+        }
+        public void RemoveLast()
+        {
+
+        }
+        public void GetNode(int x)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            
+            return this.node.value.ToString();
+        }
+    }
+}
