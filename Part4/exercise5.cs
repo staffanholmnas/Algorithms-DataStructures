bf887ff51f13e4d58c20f9d4516dd573b2f6867a
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-27 22:49:48 +0300

    Create Exercise 5

---
+++ Part4/exercise5.cs
@@ -0,0 +1,14 @@
+namespace Part4
+{
+    public class BinarySearchTree
+    {
+        public void Add(int x)
+        {
+            
+        }
+        public int Height()
+        {
+            
+        }
+    }
+}
