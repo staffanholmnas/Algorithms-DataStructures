c507ee1c97dc90fa5d097076d35cb21b44b64a35
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-28 18:40:57 +0300

    Edit Exercise 1

--- Part4/Program.cs
+++ Part4/Program.cs
@@ -7,12 +7,16 @@ namespace Part4
         static void Main(string[] args)
         {
             LinkedList myLinks = new LinkedList();
-            myLinks.AddLast(1);
+            myLinks.AddLast(4);
+            myLinks.AddLast(5);
+            myLinks.AddFirst(3);
             myLinks.AddFirst(2);
-            myLinks.AddLast(3);
+            myLinks.AddFirst(1);
+            myLinks.printNodes();
+            myLinks.AddLast(6);
             Console.WriteLine(myLinks); // for example: 2, 1, 3
-            myLinks.RemoveFirst();
-            Console.WriteLine(myLinks); // for example: 1, 3
+            //myLinks.RemoveFirst();
+            //Console.WriteLine(myLinks); // for example: 1, 3
             //Console.WriteLine(myLinks.GetNode(0)); // 1
             //Console.WriteLine(myLinks.GetNode(1)); // 3
         }
--- Part4/exercise1/LinkedList.cs
+++ Part4/exercise1/LinkedList.cs
@@ -1,9 +1,11 @@
+using System;
 
 namespace Part4
 {
     public class LinkedList
     {
-
+        // Disclaimer, I used this article for ideas on how to do this exercise: 
+        // https://www.c-sharpcorner.com/article/linked-list-implementation-in-c-sharp/
         public Node head;
 
         public LinkedList()
@@ -11,22 +13,44 @@ namespace Part4
             this.head = null;
         }
 
-        public void AddFirst(LinkedList linkedList, int n)
+        public void AddFirst(int n)
         {
-            Node newNode = new Node(n);
-            newNode.next = linkedList.head;
+            Node newNode = new Node(n, null, null);
+            newNode.next = this.head;
             newNode.previous = null;
-            if (linkedList.head != null)
+            if (this.head != null)
             {
-                linkedList.head.previous = newNode;
+                this.head.previous = newNode;
             }
-            linkedList.head = newNode;
-            
+            this.head = newNode;
+        }
+        public void printNodes()
+        {
+            Node current = this.head;
+            while (current!= null)
+            {
+                Console.WriteLine(current.value);
+                current = current.next;
+            }
         }
         public void AddLast(int n)
         {
-
+            Node newNode = new Node(n, null, null);
+            if (this.head == null)
+            {
+                newNode.previous = null;
+                this.head = newNode;
+                return;
+            }
+            Node lastNode = this.head;
+            while (lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+            lastNode.next = newNode;
+            newNode.previous = lastNode;
         }
+        
         public void RemoveFirst()
         {
 
@@ -42,9 +66,25 @@ namespace Part4
 
         public override string ToString()
         {
+            int count = 0;
+            Node current = this.head;
+            while (current!= null)
+            {
+                count++;
+                current = current.next;
+            }
+            string[] allValues = new string[count];
+            current = this.head;
+            int index = 0;
+            while (current!= null)
+            {
+                allValues[index] = current.value.ToString();
+                current = current.next;
+                index++;
+            }
             
-
-            return this.node.value.ToString();
+            string stringOfValues = String.Join(", ", allValues);
+            return stringOfValues;
         }
     }
 }
