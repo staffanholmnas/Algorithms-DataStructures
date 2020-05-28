cae0abc8d4bb362f86e00d04ad10abff149c294d
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-28 09:36:22 +0300

    Edit Exercise 1

--- Part4/exercise1/LinkedList.cs
+++ Part4/exercise1/LinkedList.cs
@@ -4,16 +4,23 @@ namespace Part4
     public class LinkedList
     {
 
-        public Node node;
+        public Node head;
 
         public LinkedList()
         {
-            this.node = new Node(0,null,null);
+            this.head = null;
         }
 
-        public void AddFirst(int n)
+        public void AddFirst(LinkedList linkedList, int n)
         {
-            this.node.value = n;
+            Node newNode = new Node(n);
+            newNode.next = linkedList.head;
+            newNode.previous = null;
+            if (linkedList.head != null)
+            {
+                linkedList.head.previous = newNode;
+            }
+            linkedList.head = newNode;
             
         }
         public void AddLast(int n)
