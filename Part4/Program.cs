f90274ea078c34140e807bb17f07007958ab6b8e
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-29 14:16:56 +0300

    Exercise 2 Done

--- Part4/Program.cs
+++ Part4/Program.cs
@@ -6,15 +6,10 @@ namespace Part4
     {
         static void Main(string[] args)
         {
-            LinkedList myLinks = new LinkedList();
-            myLinks.AddLast(1);
-            myLinks.AddFirst(2);
-            myLinks.AddLast(3);
-            Console.WriteLine(myLinks); // for example: 2, 1, 3
-            myLinks.RemoveFirst();
-            Console.WriteLine(myLinks); // for example: 1, 3
-            Console.WriteLine(myLinks.GetNode(0)); // 1
-            Console.WriteLine(myLinks.GetNode(1)); // 3
+            CircleGame g = new CircleGame();
+            Console.WriteLine(g.Last(7)); // 7
+            Console.WriteLine(g.Last(4)); // 1
+            Console.WriteLine(g.Last(123)); // 119
         }
     }
 }
--- Part4/exercise2.cs
+++ Part4/exercise2.cs
@@ -1,10 +1,27 @@
+using System;
+
 namespace Part4
 {
     public class CircleGame
     {
         public int Last(int n)
-        {
-            return 0;
+        { 
+            double power = 0;
+            int sum = 1;
+
+            for (int i = 2; i <= n; i *= 2)
+            {
+                power++;
+            }
+
+            power = Math.Pow(2, power);
+          
+            for (int i = (int)power; i < n; i++)
+            {
+                sum += 2;
+            }
+          
+            return sum;
         }
     }
 }
