29ec5eb7a3e61961227f7008d83fd64455a41a51
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-02 18:59:21 +0300

    Exercise 4 Done

--- Part4/Program.cs
+++ Part4/Program.cs
@@ -7,15 +7,10 @@ namespace Part4
     {
         static void Main(string[] args)
         {
-            BinarySearchTree bs = new BinarySearchTree();
-            bs.Add(5); // Becomes your root
-            bs.Add(4); // Goes to the left
-            bs.Add(6); // Goes to the right
-            Console.WriteLine(bs.Height()); // 1
-            bs.Add(3);
-            bs.Add(1);
-            bs.Add(7);
-            Console.WriteLine(bs.Height()); // 3
+            Order o = new Order();
+            int[] a = { 4, 2, 1, 3, 5 };
+            int[] b = { 2, 4, 3, 1, 5 };
+            o.Create(a, b); // 2 3 5 1 4
 
         }
         static void RandomNodes(BinarySearchTree tree)
--- Part4/exercise4.cs
+++ Part4/exercise4.cs
@@ -1,10 +1,55 @@
+using System;
+
 namespace Part4
 {
     public class Order
     {
-        public int[] Create(int[] a, int[] b)
+        // Due to time constraints, I got help from the internet (quite reluctantly).
+        // I modified code I found on geeksforgeeks.com, originally contributed by 29AjayKumar.
+        // This code implements recursion to print the post-order.
+        public void Create(int[] a, int[] b)
         {
-            return a;
+            printPostOrder(b, a, b.Length);
+        }
+        
+        public void printPostOrder(int[] inO, int[] pre, int n)
+        {
+            // The left and right subtrees can be found by searching for the root in inO[],
+            // which always is the first value of pre[]. So pre[0].
+            int root = search(inO, pre[0], n);
+
+            // Print the left subtree recursively.  
+            int[] ar;
+            if (root != 0)
+            {
+                ar = new int[n - 1];
+                Array.Copy(pre, 1, ar, 0, n - 1); // Copies the array to another array.
+                printPostOrder(inO, ar, root);
+            }
+
+            // Print the right subtree recursively.  
+            if (root != n - 1)
+            {
+                ar = new int[n - (root + 1)];
+                Array.Copy(inO, root + 1, ar, 0, n - (root + 1));
+                int[] ar1 = new int[n - (root + 1)];
+                Array.Copy(pre, root + 1, ar1, 0, n - (root + 1));
+                printPostOrder(ar, ar1, n - root - 1);
+            }
+
+            // Print the root.  
+            Console.Write(pre[0] + " ");
+        }
+        public int search(int[] a, int x, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
