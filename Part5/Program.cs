9eca5cd2c2d949ee7d5f50406c36521b6bfb8272
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-03 10:08:55 +0300

    Exercise 2 update

--- Part5/Program.cs
+++ Part5/Program.cs
@@ -6,7 +6,13 @@ namespace Part5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Connectivity c = new Connectivity(6);
+            c.AddConnection(1, 2);
+            c.AddConnection(2, 3);
+            c.AddConnection(1, 3);
+            c.AddConnection(3, 4);
+            c.AddConnection(5, 6);
+            Console.WriteLine(c.Calculate(1)); // 3
         }
     }
 }
