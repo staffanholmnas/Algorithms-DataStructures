6cbb213efe48e5e37257240f2a589f2aa5f2bbb7
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-03 18:59:18 +0300

    Exercise 2 Done

--- Part5/exercise2.cs
+++ Part5/exercise2.cs
@@ -1,21 +1,60 @@
+using System.Collections.Generic;
+
 namespace Part5
 {
     public class Connectivity
     {
         public int n;
+        public List<int>[] graph;
 
         public Connectivity(int n)
         {
             this.n = n;
+            this.graph = new List<int>[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List<int>();
+            }
         }
 
         public void AddConnection(int a, int b)
         {
+            graph[a].Add(b);
+            graph[b].Add(a);
 
         }
         public int Calculate(int x)
         {
-            return 0;
+            bool[] visited = new bool[n + 1];
+
+            Search(x, visited);
+
+            int sum = 0;
+
+            foreach (bool item in visited)
+            {
+                if (item == true)
+                {
+                    sum++;
+                }
+            }
+            return sum - 1; // The node has visited itself, but we don't count this, so 1 is subtracted. 
+
+        }
+        public void Search(int x, bool[] visited)
+        {
+            if (visited[x] == true)
+            {
+                return;
+            }
+
+            visited[x] = true;
+
+            foreach (int i in graph[x])
+            {
+                Search(i, visited);
+            }
         }
     }
 }
