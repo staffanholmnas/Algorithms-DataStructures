0c2ee568f50723d866d574c89415448429d94d18
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-03 21:08:15 +0300

    Exercise 3 Done

--- Part5/Program.cs
+++ Part5/Program.cs
@@ -6,13 +6,13 @@ namespace Part5
     {
         static void Main(string[] args)
         {
-            Connectivity c = new Connectivity(6);
-            c.AddConnection(1, 2);
-            c.AddConnection(2, 3);
-            c.AddConnection(1, 3);
-            c.AddConnection(3, 4);
-            c.AddConnection(5, 6);
-            Console.WriteLine(c.Calculate(1)); // 3
+            Components k = new Components(6);
+            k.AddConnection(1, 2);
+            k.AddConnection(2, 3);
+            k.AddConnection(1, 3);
+            k.AddConnection(3, 4);
+            k.AddConnection(5, 6);
+            Console.WriteLine(k.Calculate()); // 2
         }
     }
 }
--- Part5/exercise3.cs
+++ Part5/exercise3.cs
@@ -1,21 +1,60 @@
+using System.Collections.Generic;
+
 namespace Part5
 {
     public class Components
     {
         public int n;
+        public List<int>[] graph;
 
         public Components(int n)
         {
             this.n = n;
+            this.graph = new List<int>[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List<int>();
+            }
         }
 
         public void AddConnection(int a, int b)
         {
-
+            graph[a].Add(b);
+            graph[b].Add(a);
         }
         public int Calculate()
         {
-            return 0;
+            bool[] visited = new bool[n];
+
+            int searchMe = 1, sum = 0;
+
+            foreach (bool item in visited)
+            {
+                if (item == false)
+                {
+                    Search(searchMe, visited);
+                    sum++;
+                }
+                searchMe++;
+            }
+            
+            return sum;
+        }
+
+        public void Search(int x, bool[] visited)
+        {
+            if (visited[x - 1] == true)
+            {
+                return;
+            }
+
+            visited[x - 1] = true;
+
+            foreach (int i in graph[x])
+            {
+                Search(i, visited);
+            }
         }
     }
 }
