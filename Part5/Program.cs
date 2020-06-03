a78d52f1e02821658dd1abbb64992a1687afdc2f
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-03 22:29:25 +0300

    Exercise 4 Done

--- Part5/Program.cs
+++ Part5/Program.cs
@@ -6,13 +6,15 @@ namespace Part5
     {
         static void Main(string[] args)
         {
-            Components k = new Components(6);
-            k.AddConnection(1, 2);
-            k.AddConnection(2, 3);
-            k.AddConnection(1, 3);
-            k.AddConnection(3, 4);
-            k.AddConnection(5, 6);
-            Console.WriteLine(k.Calculate()); // 2
+            Communication com = new Communication(6);
+            com.AddConnection(1, 2);
+            com.AddConnection(2, 3);
+            com.AddConnection(1, 3);
+            com.AddConnection(3, 4);
+            com.AddConnection(5, 6);
+            Console.WriteLine(com.Examine(1, 4)); // true
+            Console.WriteLine(com.Examine(2, 5)); // false
+            Console.WriteLine(com.Examine(5, 6)); // true
         }
     }
 }
--- Part5/exercise4.cs
+++ Part5/exercise4.cs
@@ -1,21 +1,50 @@
+using System.Collections.Generic;
+
 namespace Part5
 {
     public class Communication
     {
         public int n;
+        public List<int>[] graph;
 
         public Communication(int n)
         {
             this.n = n;
+            this.graph = new List<int>[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List<int>();
+            }
         }
 
         public void AddConnection(int a, int b)
         {
-
+            graph[a].Add(b);
+            graph[b].Add(a);
         }
         public bool Examine(int x, int y)
         {
-            return false;
+            bool[] visited = new bool[n];
+
+            Search(x, visited);
+           
+            return visited[y - 1];
+        }
+
+        public void Search(int x, bool[] visited)
+        {
+            if (visited[x - 1] == true)
+            {
+                return;
+            }
+
+            visited[x - 1] = true;
+
+            foreach (int i in graph[x])
+            {
+                Search(i, visited);
+            }
         }
     }
 }
