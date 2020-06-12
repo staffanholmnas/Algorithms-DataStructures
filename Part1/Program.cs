bacb6a46510c9fc5407d2d9815c7a2668bc601b2
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-12 16:56:19 +0300

    Part 1 Done

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -8,6 +8,7 @@ namespace Part1
         {
             LuckyNumbers luck = new LuckyNumbers();
             Console.WriteLine(luck.Calculate(1, 10)); // 2
+            Console.WriteLine(luck.Calculate(1, 10)); // 2
             Console.WriteLine(luck.Calculate(123, 321)); // 0
             Console.WriteLine(luck.Calculate(1, 1000000)); // 126
 
