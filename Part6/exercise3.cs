176163fd138eb4b95eb511aeb6e11e4ddd54b068
staffanholmnas <staffan.holmnas@centria.fi>
2020-06-17 14:18:22 +0300

    Exercise 3 Done and improved.

--- Part6/exercise3.cs
+++ Part6/exercise3.cs
@@ -1,74 +1,62 @@
 namespace Exercise3
+
 {
     public class ShortestPath
     {
         public int n;
-        public int[,] graph;
+        public int[,] distance;
+        public int max = 999999999;
+        public bool calculated = false;
 
         public ShortestPath(int n)
         {
             this.n = n;
-            this.graph = new int[n + 1, n + 1];
+            this.distance = new int[this.n + 1, this.n + 1];
+
+            // Add infinity to all edges in distance matrix instead of zeros. Distance[node,node] = 0. 
+            for (int i = 1; i <= this.n; i++)
+            {
+                for (int j = 1; j <= this.n; j++)
+                {
+                    this.distance[i, j] = max;
+
+                    if (i == j)
+                    {
+                        this.distance[i, j] = 0;
+                    }
+                }
+            }
         }
 
         public void AddRoad(int a, int b, int d)
         {
-            graph[a, b] = d;
-            graph[b, a] = d;
+            this.distance[a, b] = d;
+            this.distance[b, a] = d;
         }
         public int Calculate(int x, int y)
         {
             // Floyd-Warshall algorithm.
-            int[,] distance = new int[n + 1, n + 1];
-            int max = int.MaxValue;
 
-            // Copy matrix to distance matrix, add infinity instead of zeros. Distance[node,node] = 0. 
-            for (int i = 0; i < n + 1; i++)
+            if (this.calculated == false) // Only calculate if needed.
             {
-                for (int j = 0; j < n + 1; j++)
+                for (int k = 1; k <= this.n; k++)
                 {
-                    distance[i, j] = graph[i,j];
-                    if (graph[i,j] == 0)
+                    for (int i = 1; i <= this.n; i++)
                     {
-                        distance[i,j] = max;
-                    }
-                    if (i == j)
-                    {
-                        distance[i,j] = 0;
-                    }
-                }
-            }
-
-            // Algorithm.
-            for (int k = 0; k < n + 1; k++)
-            {
-                for (int i = 0; i < n + 1; i++)
-                {
-                    for (int j = 0; j < n + 1; j++)
-                    {
-                        if (distance[i,k] != max && distance[k,j] != max &&
-                        distance[i,k] + distance[k,j] < distance[i,j])
-                        {
-                            distance[i,j] = distance[i,k] + distance[k,j];
+                        for (int j = 1; j <= this.n; j++)
+                        { 
+                            this.distance[i, j] = System.Math.Min(this.distance[i, j], this.distance[i, k] + this.distance[k, j]);
                         }
                     }
                 }
+
+                this.calculated = true;
             }
 
-            // Print the distance.
-            int print = 0;
-            for (int i = 0; i < n + 1; i++)
-            {
-                for (int j = 0; j < n + 1; j++)
-                {
-                    if (i == x && j == y)
-                    {
-                        print = distance[i,j];
-                    }
-                }
-            }
+            // Return the distance.
+            if (this.distance[x, y] < max) return this.distance[x, y];
 
-            return print;
+            return -1;
         }
     }
 }
